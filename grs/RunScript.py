4a853c6e719be4c16591bd947bd9cde40e6b9813
Anthony G. Basile <blueness@gentoo.org>
2015-07-07 08:57:22 -0400

    grs/RunScript.py: remove .sh from script since either python or bash will do.

--- grs/RunScript.py
+++ grs/RunScript.py
@@ -20,9 +20,9 @@ class RunScript():
 
     def runscript(self, script_name):
         script_org = os.path.join(self.libdir, 'scripts/%s' % script_name)
-        script_dst = os.path.join(self.portage_configroot, 'tmp/script.sh')
+        script_dst = os.path.join(self.portage_configroot, 'tmp/script')
         shutil.copy(script_org, script_dst)
         os.chmod(script_dst, 0o0755)
-        cmd = 'chroot %s /tmp/script.sh' % self.portage_configroot
+        cmd = 'chroot %s /tmp/script' % self.portage_configroot
         Execute(cmd, timeout=None, logfile=self.logfile)
         os.unlink(script_dst)
